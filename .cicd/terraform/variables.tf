7d0a770aa8fc428853a5ca0fdb20b202d55fdf98
pradeeptathineni <pradeep.tathineni@gmail.com>
2023-04-15 16:58:15 -0400

    Fix: Removed aws cred vars from cicd variables.tf

--- .cicd/terraform/variables.tf
+++ .cicd/terraform/variables.tf
@@ -31,14 +31,6 @@ variable "circleci_token" {
   type = string
 }
 
-variable "aws_access_key_id" {
-  type = string
-}
-
-variable "aws_secret_access_key" {
-  type = string
-}
-
 variable "ecr_repository_url" {
   type = string
 }
